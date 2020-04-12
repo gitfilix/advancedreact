e0eec07023433f6f95bac04f7325f39c06aa7304
gitfilix <runlevel07@gmail.com>
2020-04-12 14:28:23 +0200

    backend server working with no queries

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,22 @@
 // entry point of application - let's go!
 require('dotenv').config({ path: 'variables.env'})
-const createServer = require('createServer')
+// its not a package it is a (config-)file
+const createServer = require('./createServer')
 const db = require('./db')
 
+// a version of graphql -yoga server
 const server = createServer()
 
+// TODO Use express middleware to handle cookies
+// TODO Use express middleware to handle cookies
 
-// TODO Use express middleware to handle cookies
-// TODO Use express middleware to handle cookies
+// HERE we connect to our frontend
+  server.start({
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  }, deets => {
+      console.log(`BackendServer is now running on port http://localhost${deets.port}`)
+  }
+)
