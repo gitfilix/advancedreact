8af44d9a3dfe7a1fc034aaafed0b921cda006026
gitfilix <runlevel07@gmail.com>
2020-04-12 17:48:20 +0200

    direct forwarded query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,6 +1,10 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
+  // forwarded directly to prisma
+  // items: forwardTo('db')
+
+  // customized way
   async items(parent, args, ctx, info) {
     const items = await ctx.db.query.items()
     return items;
