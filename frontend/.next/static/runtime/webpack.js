2ee00d17f3bbaaed4c91b4f767c7ed8f6c10656c
gitfilix <runlevel07@gmail.com>
2020-04-13 14:42:52 +0200

    ar -  unneccessary .next files may they go away

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e141888c1acdbdb04365";
+/******/ 	var hotCurrentHash = "d5e3dd9397529d738a37";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
