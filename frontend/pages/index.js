36a53d51a5d94bdbb345cbebef3f191376f2b6ef
gitfilix <runlevel07@gmail.com>
2020-04-13 14:42:07 +0200

    ar - frontend load all items in item component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Home = props => (
   <>
-      <p>Hey there index!</p>
+      <p>Hey !</p>
   </>
 
 )
---
+++ frontend/pages/items.js
@@ -0,0 +1,12 @@
+// import Home from './index'
+import Items from '../components/Items'
+
+const Home = props => (
+  <>
+    <Items />
+  </>
+
+)
+
+
+export default Home
